2de057a8f38ee0cbbfd77547c0509347e1301e82
kristin <kes22@hi.is>
2021-11-16 13:45:30 +0000

    Again..

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+//Tuesday november
+
+
+
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-//junk comment line 5
+//Sheriff
+
+//Hello World
+
 #include "main.h"
 
 #include "addrman.h"
